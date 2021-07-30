e48594eaf1c7c179eecfbf9dc2d77f3d8a600fa1
Maria <girlfrombaikal@gmail.com>
2021-07-30 10:27:36 +0300

    added M6 homework (task 2)

--- 06_PrimitiveDataTypes/IntOrInteger/src/ContainerTest.java
+++ 06_PrimitiveDataTypes/IntOrInteger/src/ContainerTest.java
@@ -4,9 +4,11 @@ public class ContainerTest {
         container.addCount(5672);
         System.out.println(container.getCount());
 
-        // TODO: ниже напишите код для выполнения задания:
-        //  С помощью цикла и преобразования чисел в символы найдите все коды
-        //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
-
+        for (int i = 1040; i < 1106; i++){
+            if (i == 1104) continue;
+            char c = (char) i;
+            System.out.println(i + " - " + c);
+        }
+        System.out.println("1025 - " + (char) 1025);
     }
 }
