c269c8867de698efcc2fb8be8c7b7bb479a67053
Maria <girlfrombaikal@gmail.com>
2021-07-31 21:52:44 +0300

    added M7 homework (task 2)

--- 07_NumbersAndDates/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 07_NumbersAndDates/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -5,20 +5,37 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String boxes = scanner.nextLine();
+        int numberBoxes = Integer.parseInt(boxes);
+        int boxesInContainer = 27;
+        int containersInTruck = 12;
+        int boxesInTruck = boxesInContainer * containersInTruck;
+        int numberContainers = ((numberBoxes % boxesInContainer) == 0) ? numberBoxes / boxesInContainer : 1 + (numberBoxes / boxesInContainer);
+        int numberTrucks = (numberBoxes % boxesInTruck) == 0 ? numberBoxes / boxesInTruck : 1 + numberBoxes / boxesInTruck;
 
-        // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
-        // пример вывода при вводе 2
-        // для отступа используйте табуляцию - \t
+        int k = 1;
+        int j = 1;
+        for (int i = 1; i <= numberTrucks; i++) {
+            System.out.println("Грузовик: " + i);
+            for (; j <= numberContainers; j++) {
+                System.out.println("\tКонтейнер: " + j);
+                for (; k <= numberBoxes; k++) {
+                    System.out.println("\t\tЯщик: " + k);
+                    if (k % 27 == 0) {
+                        k++;
+                        break;
+                    }
 
-        /*
-        Грузовик: 1
-            Контейнер: 1
-                Ящик: 1
-                Ящик: 2
-        Необходимо:
-        грузовиков - 1 шт.
-        контейнеров - 1 шт.
-        */
+                }
+                if (j % 12 == 0) {
+                    j++;
+                    break;
+                }
+            }
+        }
+
+        System.out.println("Необходимо:" + System.lineSeparator()
+                + "грузовиков - " + numberTrucks + " шт." + System.lineSeparator()
+                + "контейнеров - " + numberContainers + " шт." + System.lineSeparator());
+
     }
-
 }
