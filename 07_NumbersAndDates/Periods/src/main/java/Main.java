33d1ad13f6796608a5e1177e59ba30e8ec9568ee
Maria <girlfrombaikal@gmail.com>
2021-08-03 13:14:58 +0300

    added M7-2 homework (task 2)

--- 07_NumbersAndDates/Periods/src/main/java/Main.java
+++ 07_NumbersAndDates/Periods/src/main/java/Main.java
@@ -1,13 +1,21 @@
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Main {
 
   public static void main(String[] args) {
 
+    LocalDate birthday = LocalDate.of(1996, 1, 21);
+    System.out.print(getPeriodFromBirthday(birthday));
   }
 
   private static String getPeriodFromBirthday(LocalDate birthday) {
-    return "";
+
+    int years = Period.between(birthday, LocalDate.now()).getYears();
+    int months = Period.between(birthday, LocalDate.now()).getMonths();
+    int days = Period.between(birthday, LocalDate.now()).getDays();
+
+    return years + " years, " + months + " months, " + days + " days";
   }
 
 }
