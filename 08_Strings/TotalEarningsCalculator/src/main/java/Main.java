fb5f28bff1012787af01234f1275bb7fc9313009
Maria <girlfrombaikal@gmail.com>
2021-08-06 12:19:06 +0300

    added M8 homework (task 1)

--- 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
@@ -3,7 +3,19 @@ public class Main {
   public static void main(String[] args) {
 
     String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    //TODO: напишите ваш код, результат вывести в консоль
-  }
 
+    int start = 0, end = 0, sum=0;
+    String rub = "руб";
+
+    for (int i=0; i<3; i++){
+      end = text.indexOf(rub, end+rub.length())-1;
+      for (int j = end-1; text.charAt(j) != ' '; j--){
+        start = j;
+      }
+      sum += Integer.parseInt(text.substring(start, end));
+    }
+    System.out.println(sum);
+  }
 }
+
+
