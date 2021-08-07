20521d16c215467d7ab60e1baf4c53882a626d16
Maria <girlfrombaikal@gmail.com>
2021-08-07 15:48:03 +0300

    added M8 homework (task 5)

--- 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
+++ 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
@@ -2,17 +2,29 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
+            if (isValidName(input)) printName(input);
+        }
     }
-  }
 
+    public static boolean isValidName(String input) {
+        String symbolsForName = "[АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя-]+";
+        String regex = symbolsForName + "[ ]+" + symbolsForName + "[ ]+" + symbolsForName;
+        if (!input.matches(regex)) System.out.println("Введенная строка не является ФИО");
+        return input.matches(regex);
+    }
+
+    public static void printName(String input) {
+        String[] words = input.split(" +");
+        System.out.println("Фамилия: " + words[0] + System.lineSeparator());
+        System.out.println("Имя: " + words[1] + System.lineSeparator());
+        System.out.println("Отчество: " + words[2] + System.lineSeparator());
+    }
 }
