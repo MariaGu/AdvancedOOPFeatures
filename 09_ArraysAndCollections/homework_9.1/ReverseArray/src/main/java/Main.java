22d3747e07957bd34e6f14f1c6fe78ef5501f988
Maria <girlfrombaikal@gmail.com>
2021-08-08 16:04:41 +0300

    added M9-1 homework (task 1)

--- 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
+
         String line = "Каждый охотник желает знать, где сидит фазан";
+        String[] words = line.split(",?\\s+");
+        words = ReverseArray.reverse(words);
+        for (String word: words){
+            System.out.println(word);
+        }
     }
 }
--- 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/ReverseArray.java
+++ 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/ReverseArray.java
@@ -1,7 +1,11 @@
 public class ReverseArray {
 
-    //TODO: Напишите код, который меняет порядок расположения элементов внутри массива на обратный.
     public static String[] reverse (String[] strings){
+        for (int i = 0; i < strings.length/2; i++){
+            String temp = strings[strings.length - i -1];
+            strings[strings.length - i -1] = strings[i];
+            strings[i] = temp;
+        }
         return strings;
     }
 }
