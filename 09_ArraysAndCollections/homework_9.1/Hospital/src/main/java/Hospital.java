0cdba4ada6511ffb608b1ace8feed44b74acebdd
Maria <girlfrombaikal@gmail.com>
2021-08-08 17:32:26 +0300

    added M9-1 homework (task 2)

--- 09_ArraysAndCollections/homework_9.1/Hospital/src/main/java/Hospital.java
+++ 09_ArraysAndCollections/homework_9.1/Hospital/src/main/java/Hospital.java
@@ -2,21 +2,43 @@ public class Hospital {
 
     public static float[] generatePatientsTemperatures(int patientsCount) {
 
-        //TODO: напишите метод генерации массива температур пациентов
+        int minTemperature = 32;
+        int maxTemperature = 40;
 
-        return new float[0];
+        float[] temperatureData = new float[patientsCount];
+        for (int i = 0; i<patientsCount; i++){
+            temperatureData[i] =
+                    (float) Math.random() * (maxTemperature - minTemperature) + minTemperature;
+        }
+        return temperatureData;
     }
 
     public static String getReport(float[] temperatureData) {
-        /*
-        TODO: Напишите код, который выводит среднюю температуру по больнице,количество здоровых пациентов,
-            а также температуры всех пациентов.
-        */
+
+        int healthyPatients = 0;
+        float summedTemperature = 0;
+        float minHealthyTemperature = 36.2f;
+        float maxHealthyTemperature = 36.9f;
+        for (float temperature: temperatureData){
+            if (temperature >= minHealthyTemperature && temperature <=maxHealthyTemperature){
+                healthyPatients++;
+            }
+            summedTemperature += temperature;
+        }
+
+        String[] stringData = new String[temperatureData.length];
+        for (int i = 0; i<temperatureData.length; i++){
+            temperatureData[i] = Math.round(temperatureData[i]*10)/10.f;
+            stringData[i] = Float.toString(temperatureData[i]);
+        }
+        String temperatures = String.join(" ", stringData);
+
+        float averageTemperature = summedTemperature/ temperatureData.length;
 
         String report =
-                "Температуры пациентов: " + 0 +
-                        "\nСредняя температура: " + 0 +
-                        "\nКоличество здоровых: " + 0;
+                "Температуры пациентов: " + temperatures +
+                        "\nСредняя температура: " + Math.round(averageTemperature*100)/100.00f +
+                        "\nКоличество здоровых: " + healthyPatients;
 
         return report;
     }
