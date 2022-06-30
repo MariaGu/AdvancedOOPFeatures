d23a28d8527c7eaf11164c2753ca86b1f93aac24
gubina_mariia <girlfrombaikal@gmail.com>
2022-06-30 19:01:49 +0300

    homework 13-12. task 3 is done

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,17 +1,34 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import java.util.Collections;
-import java.util.List;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
+
+    public Main() {
+    }
+
     public static void main(String[] args) {
 
+    }
 
+    public static boolean isFlightOk(Flight flight) {
+
+        LocalDateTime flightTime = Instant.ofEpochMilli(flight.getDate().getTime())
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime();
+        return (flight.getType() == Flight.Type.DEPARTURE)
+                && (flightTime.isAfter(LocalDateTime.now()))
+                && (flightTime.isBefore(LocalDateTime.now().plusHours(2)));
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
-    }
 
+        return airport.getTerminals().stream().flatMap(terminal -> terminal.getFlights().stream()).filter(Main::isFlightOk).collect(Collectors.toList());
+
+    }
 }
